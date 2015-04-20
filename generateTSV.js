7f092088d910760092d53e685c3ccb6e8f6a4d92
Michaël Zasso <mic.besace@gmail.com>
2015-04-20 17:12:08 +0200

    use async methods to get around memory leak of gunzipSync

--- generateTSV.js
+++ generateTSV.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var gzip = require('zlib');
 var glob = require('glob');
 var join = require('path').join;
+var async = require('async');
 
 var parse = require('./src/parser');
 var getFingerprint = require('./src/fingerprint');
@@ -17,15 +18,22 @@ glob('**/*.gz', {
         console.error(err);
         process.exit(1);
     }
-    files.forEach(treatFile);
+    async.eachSeries(files, treatFile, function (err) {
+        if (err) console.log(err);
+    });
 });
 
-function treatFile(file) {
-    var data = fs.readFileSync(join(config.data, file));
-    var contents = gzip.gunzipSync(data).toString();
-    var protein = parse(contents);
-    if (protein.experiment.indexOf('DIFFRACTION') > 0) {
-        var fingerprint = getFingerprint(protein.atoms);
-        process.stdout.write(protein.idCode + '\t' + fingerprint.join('\t') + '\n');
-    }
+function treatFile(file, cb) {
+    fs.readFile(join(config.data, file), function (err, data) {
+        if (err) return cb(err);
+        gzip.gunzip(data, function (err, contents) {
+            if (err) return cb(err);
+            var protein = parse(contents.toString());
+            if (protein.experiment.indexOf('DIFFRACTION') > 0) {
+                var fingerprint = getFingerprint(protein.atoms);
+                process.stdout.write(protein.idCode + '\t' + fingerprint.join('\t') + '\n');
+                cb();
+            }
+        });
+    });
 }
