771200e5d795b43f5d650ec0858e4bdabad13d5a
Michaël Zasso <mic.besace@gmail.com>
2015-05-04 08:25:47 +0200

    correct fingerprint

--- src/fingerprint.js
+++ src/fingerprint.js
@@ -13,17 +13,28 @@ function getFingerprint(atoms) {
     //console.log(atoms[1].normCoordinates);
     var eigenVectors = getEigenvectors(atoms);
     rotateCoordinates(atoms, eigenVectors);
+    var nPMI = getPMI(atoms);
     var grid = getGrid(atoms);
     var result = getHistograms(grid);
-    var finalResult = result[0].normalized.concat(
+    var avgTogc = getDensity(grid);
+
+    var finalFP = result[0].normalized.concat(
         result[1].normalized,
         result[2].normalized,
         result[3].normalized
     );
-    for (var i = 0; i < finalResult.length; i++) {
-        finalResult[i] = (finalResult[i] * 100) | 0;
+    for (var i = 0; i < finalFP.length; i++) {
+        finalFP[i] = (finalFP[i] * 100) | 0;
     }
-    return finalResult;
+
+    return finalFP.concat(
+        result[0].atomCount,
+        result[1].atomCount,
+        result[2].atomCount,
+        result[3].atomCount,
+        nPMI,
+        avgTogc
+    );
 }
 
 module.exports = getFingerprint;
@@ -96,6 +107,16 @@ function getEigenvectors(atoms) {
     var eigenVectors = evd.eigenvectorMatrix;
     var eigenValues = evd.realEigenvalues;
 
+    // switch column to row
+    for (i = 0; i < 3; i++) {
+        for (j = i + 1; j < 3; j++) {
+            var tmp = 0;
+            tmp = eigenVectors[i][j];
+            eigenVectors[i][j] = eigenVectors[j][i];
+            eigenVectors[j][i] = tmp;
+        }
+    }
+
     // If a vector is fully negative, we need to invert the values
     for (i = 0; i < 3; i++) {
         var negative = true;
@@ -157,7 +178,7 @@ function rotateCoordinates(atoms, eigenVectors) {
     }
 
     //console.log('TRANSPOSE');
-    //console.log(atoms[1].newCoordinates);
+    //console.log(atoms[0].newCoordinates);
 }
 
 /**
@@ -191,14 +212,14 @@ function getMaxima(atoms) {
             result.z[1] = atomCoord[2];
         }
     }
-    
+
     result.x[0] -= 0.5;
     result.x[1] += 0.5;
     result.y[0] -= 0.5;
     result.y[1] += 0.5;
     result.z[0] -= 0.5;
     result.z[1] += 0.5;
-    
+
     return result;
 }
 
@@ -291,7 +312,12 @@ function GridBox(x, y, z) {
     this.xGrid = x;
     this.yGrid = y;
     this.zGrid = z;
-    this.coordinates = new Array(3);
+    this.coordinates = [
+        new Array(3), //all
+        new Array(3), //pos
+        new Array(3), //neg
+        new Array(3) //hyd
+    ];
     this.prop = [0, 0, 0, 0];
     this.atoms = [];
 }
@@ -304,30 +330,42 @@ function getHistograms(grid) {
         new Result()  // hyd
     ];
 
-    var i, j;
-    var box, sum, atomL, atom;
+    var i, j, k;
+    var box, sum, sumPos, sumNeg, sumHyd, atomL, atom;
     var l = grid.length;
 
     for (i = 0; i < l; i++) {
         box = grid[i];
         if (box.atoms.length) {
-            sum = [0, 0, 0];
+            sum = [
+                [0, 0, 0], //all
+                [0, 0, 0], //pos
+                [0, 0, 0], //neg
+                [0, 0, 0] //hyd
+            ];
+
             atomL = box.atoms.length;
             for (j = 0; j < atomL; j++) {
                 atom = box.atoms[j];
-                sum[0] += atom.newCoordinates[0];
-                sum[1] += atom.newCoordinates[1];
-                sum[2] += atom.newCoordinates[2];
+                sum[0][0] += atom.newCoordinates[0];
+                sum[0][1] += atom.newCoordinates[1];
+                sum[0][2] += atom.newCoordinates[2];
                 box.prop[0]++;
                 result[0].atomCount++;
                 if (atom.kind > 0) {
                     box.prop[atom.kind]++;
                     result[atom.kind].atomCount++;
+                    sum[atom.kind][0] += atom.newCoordinates[0];
+                    sum[atom.kind][1] += atom.newCoordinates[1];
+                    sum[atom.kind][2] += atom.newCoordinates[2];
                 }
             }
-            box.coordinates[0] = sum[0] / atomL;
-            box.coordinates[1] = sum[1] / atomL;
-            box.coordinates[2] = sum[2] / atomL;
+
+            for (k = 0; k < 4; k++) {
+                box.coordinates[k][0] = sum[k][0] / box.prop[k];
+                box.coordinates[k][1] = sum[k][1] / box.prop[k];
+                box.coordinates[k][2] = sum[k][2] / box.prop[k];
+            }
         }
     }
 
@@ -337,10 +375,11 @@ function getHistograms(grid) {
         for (j = i + 1; j < l; j++) {
             var otherBox = grid[j];
             if (otherBox.atoms.length === 0) continue;
-            var distance = euclidean(box.coordinates, otherBox.coordinates);
 
-            for (var k = 0; k < 4; k++) {
+            for (k = 0; k < 4; k++) {
                 if (box.prop[k] > 0 && otherBox.prop[k] > 0) {
+                    var distance;
+                    distance = euclidean(box.coordinates[k], otherBox.coordinates[k]);
                     result[k].distances.push(distance);
                     result[k].weights.push(box.prop[k] * otherBox.prop[k]);
                 }
@@ -352,8 +391,8 @@ function getHistograms(grid) {
         var data = result[i];
         data.frequency = getHistogram(data.distances, data.weights);
         data.normalized = setWeights(normalize(data.frequency, data.atomCount), i);
-    }
 
+    }
     return result;
 }
 
@@ -392,7 +431,7 @@ function getHistogram(data, weight) {
         if (val > maxValue) continue;
 
         for (var i = 0; i < l; i++) {
-            frequency[i] += weight[a] * Math.exp(-((binset[i]-val)*(binset[i]-val)) / (2*(val*0.09)*(val*0.09)));
+            frequency[i] += weight[a] * Math.exp(-((binset[i] - val) * (binset[i] - val)) / (2 * (val * 0.09) * (val * 0.09)));
         }
     }
     return frequency;
@@ -413,4 +452,112 @@ function Result() {
     this.atomCount = 0;
     this.distances = [];
     this.weights = [];
+}
+
+/*
+ calculate nPMI
+ */
+function getPMI(atoms) {
+    var l = atoms.length;
+    var array = [0, 0, 0];
+    var sorted = [0, 0, 0];
+    var used = [];
+    var nPMI;
+
+    if (l == 1) {
+        sorted = [1, 1, 1];
+    }
+
+    var a = 0;
+    for (var i = 0; i < 3; i++) {
+        for (var j = (i + 1); j < 3; j++) {
+            for (var k = 0; k < l; k++) {
+                var atom = atoms[k];
+                var coordinates = atom.newCoordinates;
+                var atommass = getMass(atom.type);
+                var Xsq = coordinates[i] * coordinates[i];
+                var Ysq = coordinates[j] * coordinates[j];
+                var rsq = Xsq + Ysq;
+                array[a] += atommass * rsq;
+            }
+            a++;
+        }
+    }
+
+    var s = 0;
+    while (s < sorted.length) {
+        var lowest = Infinity;
+        var pos = -1;
+        for (var i = 0; i < array.length; i++) {
+            var found = false;
+            for (var j = 0; j < used.length; j++) {
+                if (used[j] == i) {
+                    found = true;
+                    break;
+                }
+            }
+            if (found == true) continue;
+            if (array[i] < lowest) {
+                lowest = array[i];
+                pos = i;
+            }
+        }
+        used.push(pos);
+        sorted[s] = lowest;
+        s++;
+    }
+    nPMI = getNormalizedPMI(sorted);
+    return nPMI;
+}
+
+function getNormalizedPMI(PMI) {
+    return [PMI[0] / PMI[2], PMI[1] / PMI[2]];
+}
+
+function getMass(type) {
+    var atomsets = ['C', 'N', 'O', 'S', 'P'];
+    var atommass = [12.0, 14.0, 16.0, 32.0, 15.0];
+    var atomtyp = type.substring(0, 1);
+    var mass;
+    for (var a = 0; a < atomsets.length; a++) {
+        if (atomtyp == atomsets[a]) {
+            mass = atommass[a];
+            break;
+        }
+    }
+    return mass;
+}
+
+/*
+ density calculation: average distance to the geometry center of the molecule
+ */
+function getDensity(grids) {
+    var avgTogc = 0;
+    var gc = [0, 0, 0];
+    var size = 0;
+    for (var i = 0; i < grids.length; i++) {
+        var hac = grids[i].atoms.length;
+        if (hac == 0) {
+            continue;
+        }
+        gc[0] += grids[i].coordinates[0][0] * hac;
+        gc[1] += grids[i].coordinates[0][1] * hac;
+        gc[2] += grids[i].coordinates[0][2] * hac;
+        size += hac;
+
+    }
+    for (var i = 0; i < 3; i++) {
+        gc[i] = gc[i] / size;
+    }
+    for (var i = 0; i < grids.length; i++) {
+        var hac = grids[i].atoms.length;
+        if (hac == 0) {
+            continue;
+        }
+        var dist = euclidean(grids[i].coordinates[0], gc);
+        avgTogc += dist * grids[i].atoms.length;
+    }
+
+    avgTogc = avgTogc / size;
+    return avgTogc;
 }
