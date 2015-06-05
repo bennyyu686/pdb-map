9c568155291316468586466ea5b506d81d5e5bae
Daniel Kostro <kostro.d@gmail.com>
2015-06-05 11:30:35 +0200

    Add pdb and modify test

--- test2.js
+++ test2.js
@@ -5,12 +5,15 @@ var gzip = require('zlib');
 
 var parse = require('./src/parser');
 var getFingerprint = require('./src/fingerprint');
+var mapPosition = require('./src/mapPosition');
 
-var file = fs.readFileSync('./data/vc/2AAI.pdb.gz');
+var file = fs.readFileSync('./data/vc/4ERW.pdb1.gz');
 var contents = gzip.gunzipSync(file).toString();
 
 var protein = parse(contents);
 var fingerprint = getFingerprint(protein.atoms);
+var position = mapPosition(fingerprint);
 
 console.log(protein.experiment);
 console.log(JSON.stringify(fingerprint));
+console.log('position', position);
