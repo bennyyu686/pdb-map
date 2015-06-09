c9bad517b2d047496f931c93b41bd72417451ed9
Michaël Zasso <mic.besace@gmail.com>
2015-06-09 11:48:42 +0200

    add progress bar option to generateTSV

--- generateTSV.js
+++ generateTSV.js
@@ -5,6 +5,7 @@ var gzip = require('zlib');
 var glob = require('glob');
 var join = require('path').join;
 var async = require('async');
+var ProgressBar = require('progress');
 
 var parse = require('./src/parser');
 var getFingerprint = require('./src/fingerprint');
@@ -23,22 +24,33 @@ glob('**/*.gz', {
         console.error(err);
         process.exit(1);
     }
-    async.eachSeries(files, treatFile, function (err) {
-        if (err) console.log(err);
-    });
-});
 
-function treatFile(file, cb) {
-    fs.readFile(join(config.data, file), function (err, data) {
-        if (err) return cb(err);
-        gzip.gunzip(data, function (err, contents) {
-            if (err) return cb(err);
-            var protein = parse(contents.toString());
-            if (protein.experiment.indexOf('DIFFRACTION') > 0) {
-                var fingerprint = getFingerprint(protein.atoms);
-                process.stdout.write(protein.idCode + '\t' + fingerprint.join('\t') + '\n');
-            }
-            cb();
+    if (process.argv[2] === '--progress') {
+        var bar = new ProgressBar('  generating fingerprints [:bar] :percent :etas', {
+            complete: '=',
+            incomplete: ' ',
+            width: 20,
+            total: files.length
         });
+    }
+
+    async.eachSeries(files, treatFile, function (err) {
+        if (err) console.error(err);
     });
-}
+
+    function treatFile(file, cb) {
+        fs.readFile(join(config.data, file), function (err, data) {
+            if (err) return cb(err);
+            gzip.gunzip(data, function (err, contents) {
+                if (err) return cb(err);
+                var protein = parse(contents.toString());
+                if (protein.experiment.indexOf('DIFFRACTION') > 0) {
+                    var fingerprint = getFingerprint(protein.atoms);
+                    process.stdout.write(protein.idCode + '\t' + fingerprint.join('\t') + '\n');
+                }
+                if (bar) bar.tick();
+                cb();
+            });
+        });
+    }
+});
