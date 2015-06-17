13cbc774f5378c1b48402c503e0cedfd73b3ff81
Michaël Zasso <mic.besace@gmail.com>
2015-06-17 15:31:06 +0200

    update parser

--- src/parser.js
+++ src/parser.js
@@ -14,6 +14,7 @@ function parse(pdb) {
     var pass = true;
     var hetatm = 0;
     var atm = 0;
+    var seqres = "";
     for (var i = 0; i < lines.length; i++) {
         var line = lines[i];
         var header = line.substring(0, 6);
@@ -21,6 +22,8 @@ function parse(pdb) {
             hetatm++;
         } else if (header === 'ATOM  ') {
             atm++;
+        } else if (header === 'SEQRES') {
+            seqres += line.substring(19,70).trim()+";"; //store all residues
         }
     }
     //if HETATM is more than 20% of total atoms, remove this pdb.
@@ -75,6 +78,46 @@ function parse(pdb) {
                         line.substring(17, 20).trim(), // residue
                         [parseFloat(line.substring(30, 38)), parseFloat(line.substring(38, 46)), parseFloat(line.substring(46, 54))] // coordinates
                     ));
+                }
+            } else if (header === 'HETATM') {//if the hetatm belongs to modified residue, we take it.
+                var res = line.substring(17, 20).trim();
+                if (res == 'HOH') continue;
+                if (res.length == 0) continue;
+                var resarray = seqres.trim().replace(/\s+/g,";").split(";");
+                if (resarray.indexOf(res) > -1) {
+                    //remove hydrogens
+                    var element = line.substring(76, 78).trim();
+                    var natele = false;
+                    for (var j = 0; j < eleset.length; j++) {
+                        if (element === eleset[j]) {
+                            natele = true;
+                            break;
+                        }
+                    }
+
+                    //remove alternative position
+                    var alloc = line.substring(16, 17).trim();
+                    var ins = false;
+                    if (alloc.length > 0) {
+                        if (allocCODE.length == 0) {
+                            ins = true;
+                            allocCODE.push(alloc);
+                        } else {
+                            if (alloc == allocCODE[0]) {
+                                ins = true;
+                            }
+                        }
+                    } else {
+                        ins = true;
+                    }
+
+                    if (natele && ins) {
+                        result.atoms.push(new Atom(
+                            line.substring(12, 16).trim(), // type
+                            line.substring(17, 20).trim(), // residue
+                            [parseFloat(line.substring(30, 38)), parseFloat(line.substring(38, 46)), parseFloat(line.substring(46, 54))] // coordinates
+                        ));
+                    }
                 }
             }
         }
