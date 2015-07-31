e4d6bca8e59a57f0b3297c64f30b63d498264602
Michaël Zasso <mic.besace@gmail.com>
2015-07-31 16:51:41 +0200

    add update-fp script

--- generateTSV.js
+++ generateTSV.js
@@ -22,6 +22,7 @@ program
     .option('-p, --progress', 'Show progression')
     .option('-n, --use-name', 'Use filename (XXXX pdbs)')
     .option('-o, --out [file]', 'Output in a file')
+    .option('-u, --update', 'Update fingerprints')
     .parse(process.argv);
 
 glob('**/*.pdb1.gz', {
@@ -32,9 +33,68 @@ glob('**/*.pdb1.gz', {
         process.exit(1);
     }
 
-    var out = process.stdout;
-    if (program.out) {
+    var out;
+    if (program.update) {
+        if (!program.out) {
+            throw new Error('out file parameter is mandatory for update run');
+        }
+        if (!config.rsyncHistory) {
+            throw new Error('missing rsyncHistory config value');
+        }
+
+        // We assume that the files are ordered by timestamp.
+        // There will be a problem when the timestamp has one more digit (Sat Nov 20 2286 18:46:40 GMT+0100)
+        var historyFiles = glob('*.json', {
+            cwd: config.rsyncHistory,
+            sync: true
+        }).map(function (file) {
+            return path.resolve(config.rsyncHistory, file);
+        });
+
+        // Read last fingerprint file and create a Map with PDB id as the key
+        var currentOut = fs.readFileSync(program.out, 'utf8').split(/[\r\n]+/);
+        var map = new Map();
+        currentOut.forEach(function (out) {
+            if (out.length > 0) map.set(out.substr(0, 4), out);
+        });
+        var todo = new Set();
+
+        // For each history file, if there is a deleted PDB, we remove it from the map and todolist
+        // If there is an updated PDB, it is added to the todolist
+        for (var i = 0; i < historyFiles.length; i++) {
+            var historyFile = require(historyFiles[i]);
+            for (let j = 0; j < historyFile.deleted.length; j++) {
+                map.delete(historyFile.deleted[j]);
+                todo.delete(historyFile.deleted[j]);
+            }
+            for (let j = 0; j < historyFile.updated.length; j++) {
+                map.delete(historyFile.updated[j]);
+                todo.add(historyFile.updated[j]);
+            }
+        }
+
+        // Now that the map has been filtered, write the existing PDBs
+        out = fs.createWriteStream(program.out + '.tmp');
+        for (let line of map.values()) {
+            out.write(line + '\n');
+        }
+        map.clear();
+
+        // From the globbed files, extract the ones that we need to read
+        var oldfiles = files;
+        files = [];
+        for (let entry of todo.values()) {
+            for (let file of oldfiles) {
+                if (file.toUpperCase().includes(entry)) {
+                    files.push(file);
+                }
+            }
+        }
+
+    } else if (program.out) {
         out = fs.createWriteStream(program.out);
+    } else {
+        out = process.stdout;
     }
 
     if (program.progress) {
@@ -48,6 +108,16 @@ glob('**/*.pdb1.gz', {
 
     async.eachSeries(files, treatFile, function (err) {
         if (err) console.error(err);
+        if (program.out) {
+            out.end();
+        }
+        if (program.update) {
+            fs.unlinkSync(program.out);
+            fs.renameSync(program.out + '.tmp', program.out);
+            historyFiles.forEach(function (file) {
+                fs.unlinkSync(file);
+            });
+        }
     });
 
     function treatFile(file, cb) {
